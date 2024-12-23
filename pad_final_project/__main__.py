87142b1a3e4f32b63a3675db2a1ac169ed808322
Eduardo Miralles <emirallesiz@alumni.unav.es>
2024-12-23 23:09:51 +0100

    Yahoo style

--- pad_final_project/__main__.py
+++ pad_final_project/__main__.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import krakenex
 import matplotlib.pyplot as plt
+import mplfinance as mpf
 
 from krakenex_utils import (
+    fetch_asset_pairs, 
     fetch_ohlc_data, 
-    compute_bollinger_bands
+    compute_bollinger_bands, 
+    downsample_ohlc_data
 )
 
 
@@ -15,31 +18,61 @@ def main():
     # Streamlit App
     st.title("Kraken Pair Price Chart")
 
+    available_pairs = fetch_asset_pairs(api)
+
     # User input for the trading pair
-    pair = st.text_input("Enter the Kraken trading pair (e.g., XXBTZUSD for BTC/USD):", value="XXBTZUSD")
-    interval = st.selectbox("Select the interval (minutes):", [1, 5, 15, 30, 60, 240, 1440, 21600])
+    pair = st.selectbox("Enter the Kraken trading pair (e.g., XBTUSD for BTC/USD):", available_pairs.keys())
+    interval = st.selectbox("Select the interval (minutes):", [1, 5, 15, 30, 60, 240, 1440])
+
+    pair_kraken_name = available_pairs[pair]
 
     if st.button("Fetch and Plot"):
         # Fetch the data
-        ohlc_df = fetch_ohlc_data(api, pair, interval)
-
-        # Compute Bollinger Bands
-        window = 20  # 20-period moving average
-        num_std_dev = 2  # 2 standard deviations
-        bollinger_df = compute_bollinger_bands(ohlc_df, window, num_std_dev)
+        ohlc_df = fetch_ohlc_data(api, pair_kraken_name, interval)
         
         if ohlc_df is not None:
+            # Compute Bollinger Bands
+            window = 20  # 20-period moving average
+            num_std_dev = 2  # 2 standard deviations
+            bollinger_df = compute_bollinger_bands(ohlc_df, window, num_std_dev)
+
             # Plot the data
             st.write(f"Price Chart for {pair} (Interval: {interval} minutes)")
-            fig, ax = plt.subplots(figsize=(12, 6))
-            ax.plot(ohlc_df['time'], ohlc_df['close'], label='Close Price', color='blue')
-            ax.plot(bollinger_df['time'], bollinger_df['Upper Band'], label='Upper Band', color='red')
-            ax.plot(bollinger_df['time'], bollinger_df['Lower Band'], label='Lower Band', color='red')
-            ax.plot(bollinger_df['time'], bollinger_df['SMA'], label='Middle Band', color='red')
-            ax.set_xlabel('Time')
-            ax.set_ylabel('Price')
-            ax.set_title(f'{pair} Price vs Time')
-            ax.legend()
+
+            # Plotting within Streamlit
+            ohlc_downsample_df = downsample_ohlc_data(ohlc_df, interval)
+
+            ohlc_downsample_bb_df = ohlc_downsample_df.merge(
+                bollinger_df[["SMA", "upper_band", "lower_band"]],
+                how = "left", 
+                right_index = True, 
+                left_index = True
+            )
+
+            ohlc_downsample_bb_df = ohlc_downsample_bb_df.loc[~ohlc_downsample_bb_df["SMA"].isnull(), :]
+
+            fig, axlist = mpf.plot(
+                ohlc_downsample_bb_df,
+                type = 'candlestick',
+                style = 'tradingview',
+                volume = True,
+                tight_layout = True,
+                addplot = [
+                    mpf.make_addplot(ohlc_downsample_bb_df['upper_band'], linewidths = 0.1, color='royalblue'),
+                    mpf.make_addplot(ohlc_downsample_bb_df['SMA'], color='orange'),
+                    mpf.make_addplot(ohlc_downsample_bb_df['lower_band'], linewidths = 0.1, color='royalblue'),
+                ],
+                figsize = (12, 8),
+                panel_ratios = (3, 1), 
+                returnfig = True
+            )
+
+            # axlist[0].set_xlim([ohlc_downsample_bb_df.index[0], ohlc_downsample_bb_df.index[-1]])
+            axlist[0].set_ylim([0.999*ohlc_downsample_bb_df['lower_band'].min(), 1.001*ohlc_downsample_bb_df['upper_band'].max()])
+
+            fig.subplots_adjust(hspace = 10) 
+
+            # Use Streamlit to display the plot
             st.pyplot(fig)
         else:
             st.error("Failed to fetch data. Please check the trading pair or interval.")
--- pad_final_project/krakenex_utils.py
+++ pad_final_project/krakenex_utils.py
@@ -3,8 +3,17 @@ import numpy as np
 import krakenex
 
 
+def fetch_asset_pairs(api):
+
+    response = api.query_public('AssetPairs')
+    pairs = {response['result'][key]["altname"]: key for key in response['result'].keys()}
+
+    return pairs
+
+
 def fetch_ohlc_data(api, pair, interval=60):
     """Fetch OHLC data for a given trading pair and interval."""
+
     params = {
         'pair': pair,
         'interval': interval
@@ -22,6 +31,7 @@ def fetch_ohlc_data(api, pair, interval=60):
     )
     # Cast Unix timestamp to datetime
     ohlc_df['time'] = pd.to_datetime(ohlc_df['time'], unit='s')
+    ohlc_df.set_index('time', inplace=True)  # Set 'time' as the index for plotting
     # Cast prices as floats
     price_items = ['open', 'high', 'low', 'close', 'vwap', 'volume']
     ohlc_df = ohlc_df.astype({f'{item}': 'float' for item in price_items})
@@ -31,12 +41,28 @@ def fetch_ohlc_data(api, pair, interval=60):
 
 def compute_bollinger_bands(df, window=20, num_std_dev=2):
     """Compute Bollinger Bands for a given DataFrame of OHLC data."""
+
     # Compute the rolling mean and standard deviation
     df['SMA'] = df['close'].rolling(window=window).mean()
     df['STD'] = df['close'].rolling(window=window).std()
     
     # Compute the Bollinger Bands
-    df['Upper Band'] = df['SMA'] + (num_std_dev * df['STD'])
-    df['Lower Band'] = df['SMA'] - (num_std_dev * df['STD'])
+    df['upper_band'] = df['SMA'] + (num_std_dev * df['STD'])
+    df['lower_band'] = df['SMA'] - (num_std_dev * df['STD'])
     
     return df
+
+
+def downsample_ohlc_data(ohlc_df, interval):
+
+    df_resampled = ohlc_df.resample(f'{4*interval}min').agg({
+        'open': 'first',
+        'high': 'max',
+        'low': 'min',
+        'close': 'last',
+        'vwap': 'mean',
+        'volume': 'sum',
+        'count': 'sum'
+    })
+
+    return df_resampled
